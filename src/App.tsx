35abbca59a17dba15e8f8cdf474e591800649af7
sorkofi <kinanprincipes@gmail.com>
2022-12-13 18:32:13 -0400

    added buttons and users array

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
+import { useMemo, useState } from 'react';
 import reactLogo from './assets/react.svg'
 import './styles/global.css'
 
-let contacts = [
+let users = [
   {
-    username: "mary",
-    avatar: ":D"
+    id: 0,
+    name: "fulano"
   },
   {
-    username: "john",
-    avatar: ":O"
+    id: 1,
+    name: "sicrano"
+  },
+  {
+    id: 2,
+    name: "pinco"
+  },
+  {
+    id: 3,
+    name: "palino"
   }
-];
+]
 
 function App() {
-
+  
   return(
     <div className='flex flex-row'>
       {/* Sidebar container */}
-      <div className='w-[16rem] bg-slate-500 '>
-        <ol>{contacts.map((e) => <li>{e.avatar+" "+e.username}</li>)}</ol>
+      <div className='flex flex-col w-[16rem] bg-slate-500 '>
+        <ol className='flex flex-col'>{users.map((e) =>
+          <li key={e.id}>
+            <button className='w-4/5 bg-red-400 p-2 mx-5 my-1 rounded'>{e.name}</button>
+          </li>)}
+        </ol>
       </div>
 
       {/* Content container */}
