656bf7120fa454fec72e658f712a8ace047fe1d1
sorkofi <kinanprincipes@gmail.com>
2022-12-13 21:58:41 -0400

    optimized user routes and user array

--- src/App.tsx
+++ src/App.tsx
@@ -4,65 +4,47 @@ import { Link, Route, Routes } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
 import './styles/global.css';
 
-let users = [
-  {
-    id: 0,
-    name: "fulano"
-  },
-  {
-    id: 1,
-    name: "sicrano"
-  },
-  {
-    id: 2,
-    name: "pinco"
-  },
-  {
-    id: 3,
-    name: "palino"
-  }
-]
+function User(props: any) {
+  return <h1>{props.name}</h1>;
+}
 
-function Fulano() {
-  return <h1>Fulano</h1>;
-}
-function Sicrano() {
-  return <h1>Sicrano</h1>;
-}
-function Pinco() {
-  return <h1>Pinco</h1>;
-}
-function Palino() {
-  return <h1>Palino</h1>;
-}
+let routes = [
+  {
+    route: "/fulano",
+    username: "fulano",
+  },
+  {
+    route: "/sicrano",
+    username: "sicrano",
+  },
+  {
+    route: "/pinco",
+    username: "pinco",
+  },
+  {
+    route: "/palino",
+    username: "palino",
+  },
+
+]
 
 function App() {
   
   return(
     <div className='flex flex-row'>
       {/* Sidebar container */}
-      <div className='flex flex-col w-[16rem] bg-slate-500 '>
-        <ol className='flex flex-col'>{users.map((e) =>
-          <li key={e.id}>
-            {/* <button onClick={() => {
-              
-            }} className='w-4/5 bg-red-400 p-2 mx-5 my-1 rounded'>{e.name}</button> */}
-            <nav>
-              <Link to={"/"+e.name}>
-                <button className='w-4/5 bg-red-400 p-2 mx-5 my-1 rounded'>{e.name}</button>
-              </Link>
-            </nav>
-          </li>)}
-        </ol>
+      <div className='w-[16rem] bg-slate-500 '>
+        
       </div>
 
       {/* Content container */}
       <div className='flex-auto h-screen z-[-1] bg-slate-300'>
         <Routes>
-          <Route path={"/fulano"} element={<Fulano />} />
-          <Route path={"/sicrano"} element={<Sicrano />} />
-          <Route path={"/pinco"} element={<Pinco />} />
-          <Route path={"/palino"} element={<Palino />} />
+          {
+            routes.map((route, index) => {
+              return <Route key={index} path={route.route} element={<User name={route.username} />} />
+            })
+          }
         </Routes>
         
       </div>
