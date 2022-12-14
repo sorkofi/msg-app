b3e07c0bc766c791a478184568d1f10139eb8ca2
sorkofi <kinanprincipes@gmail.com>
2022-12-13 20:30:47 -0400

    added react-router-dom route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useMemo, useState } from 'react';
-import reactLogo from './assets/react.svg'
-import './styles/global.css'
+import { Link, Route, Routes } from 'react-router-dom';
+
+import reactLogo from './assets/react.svg';
+import './styles/global.css';
 
 let users = [
   {
@@ -21,6 +23,10 @@ let users = [
   }
 ]
 
+function Root() {
+  return <h1>hello World</h1>;
+}
+
 function App() {
   
   return(
@@ -29,13 +35,21 @@ function App() {
       <div className='flex flex-col w-[16rem] bg-slate-500 '>
         <ol className='flex flex-col'>{users.map((e) =>
           <li key={e.id}>
-            <button className='w-4/5 bg-red-400 p-2 mx-5 my-1 rounded'>{e.name}</button>
+            {/* <button onClick={() => {
+              
+            }} className='w-4/5 bg-red-400 p-2 mx-5 my-1 rounded'>{e.name}</button> */}
+            <nav>
+              <Link to="/test"><button className='w-4/5 bg-red-400 p-2 mx-5 my-1 rounded'>{e.name}</button></Link>
+            </nav>
           </li>)}
         </ol>
       </div>
 
       {/* Content container */}
       <div className='flex-auto h-screen z-[-1] bg-slate-300'>
+        <Routes>
+          <Route path="/test" element={<Root />} />
+        </Routes>
         
       </div>
     </div>
@@ -43,4 +57,3 @@ function App() {
 }
 
 export default App
-
