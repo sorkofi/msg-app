9877d37d857014e46e44f901983bbaa9ed52c42c
sorkofi <kinanprincipes@gmail.com>
2023-01-01 13:09:05 -0400

    created Message component

---
+++ src/components/Message.tsx
@@ -0,0 +1,5 @@
+export function Message() {
+   return (
+      <h1>Message</h1>
+   );
+}
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export function User(props: any) {
 
-   let test = axios.get("https://api.github.com/users/diego3g/repos");
-
    return (
       <div className="w-full h-full flex flex-col-reverse mx-5">
          
          <div>
 
-            <h1>{props.username}</h1>
+            {
+               
+            }
             <input 
                type="text" 
                className="relative m-2 rounded px-2 py-2" 
