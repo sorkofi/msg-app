3ce6fda2202b18509e99ff57d842f6ccfe12a83d
sorkofi <kinanprincipes@gmail.com>
2023-01-01 13:01:43 -0400

    refactored Sidebar component styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,46 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Sidebar(props: any) {
-    const [expanded, setExpanded] = useState<boolean>(false);
-
     return (
         <>
         
             <div className='w-16 h-full flex flex-col items-center bg-gray-900 text-gray-800 text-md'>
                 
                 {
-                props.usernames.map((route: any, index: number) => {
-                    return (
-                        <>
-                            <button 
-                                onClick={() => {
-                                    if(expanded) {
-                                        setExpanded(false);
-                                        return;
-                                    }
-                                    setExpanded(true);
-                                    }}
-                                className='rounded-[24px] transition-all hover:rounded-[15px] hover:bg-slate-400 bg-gray-400 text-center w-12 h-12 mx-4 my-2'>
-                                {route.username}
-                            </button>
-                        </>
-                    );
-                })
+                    props.usernames.map((route: any, index: number) => {
+                        return (
+                            <div className="flex">
+                                <Link to={index.toString()}>
+                                    <button className="rounded-[24px] transition-all hover:rounded-[15px] hover:bg-slate-400 bg-gray-400 text-center w-12 h-12 mx-4 my-2">{route.username}</button>
+                                </Link>
+                            </div>
+                        );
+                    })
                 }
             </div>
-
-            {/* { expanded && 
-                <div className="transition-transform w-80 h-screen bg-red-200">
-                    {
-                        props.usernames.map((e: any, index: number) => {
-                            return <Link className="flex" to={index.toString()}>
-                                <button>{e.username}</button>
-                            </Link>;
-                        })
-                    }
-                </div>
-                
-            } */}
         </>
     );
 }
